099ab8277f15a01f7ccd8170e7f9aa8646ad4062
Oluwapelumi Oyenuga <oluwapelumioyenuga@gmail.com>
2023-10-24 17:00:37 +0100

    register endpoint

--- RunGroup WebApp (learning MVC)/Controllers/AccountController.cs
+++ RunGroup WebApp (learning MVC)/Controllers/AccountController.cs
@@ -11,6 +11,7 @@ namespace RunGroup_WebApp__learning_MVC_.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ApplicationDbContext _context;
+
         public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
         {
             _userManager = userManager;
@@ -47,6 +48,45 @@ namespace RunGroup_WebApp__learning_MVC_.Controllers
             }
             TempData["Error"] = "Wrong Credentials, try again";
             return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            var register = new RegisterViewModel();
+            return View(register);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid)
+                return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new AppUser
+            {
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress,
+            };
+
+            var userResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+            if (userResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return RedirectToAction("Index","Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Race");
         }
     }
 }
---
+++ RunGroup WebApp (learning MVC)/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using RunGroup_WebApp__learning_MVC_.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace RunGroup_WebApp__learning_MVC_.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Confirm password")]
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
